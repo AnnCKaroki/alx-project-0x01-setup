13f9ee3ac3e4750a5eb5d2f8fb84a4c6cb32407b
Ann <annkarokiadmissions@gmail.com>
2025-07-20 17:51:50 +0300

    Use typescript and add dynamic rendering

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,80 +1,54 @@
 
-import React, { useState } from 'react';
-import { UserProps } from '@/interfaces';
-import UserCard from '../../components/common/UserCard';
+import UserCard from "@/components/common/UserCard";
+import UserModal from "@/components/common/UserModal";
+import Header from "@/components/layout/Header";
+import { UserPageProps, UserProps, UserData } from "@/interfaces";
+import { useState } from "react";
 
+const Users = ({ posts }: UserPageProps) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<UserData | null>(null);
 
-const Users: React.FC = () => {
-
-  const [users] = useState<UserProps[]>([
-    {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: { lat: "-37.3159", lng: "81.1496" }
-      },
-      phone: "1-770-736-8031 x56442",
-      website: "hildegard.org",
-      company: {
-        name: "Romaguera-Crona",
-        catchPhrase: "Multi-layered client-server neural-net",
-        bs: "harness real-time e-markets"
-      }
-    },
-    {
-      id: 2,
-      name: "Ervin Howell",
-      username: "Antonette",
-      email: "Shanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        suite: "Suite 879",
-        city: "Wisokyburgh",
-        zipcode: "90566-7771",
-        geo: { lat: "-43.9509", lng: "-34.4618" }
-      },
-      phone: "010-692-6593 x09125",
-      website: "anastasia.net",
-      company: {
-        name: "Deckow-Crist",
-        catchPhrase: "Proactive didactic contingency",
-        bs: "synergize scalable supply-chains"
-      }
-    }
-  ]);
-
-
+  const handleAddPost = (newPost: UserData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-extrabold text-center text-gray-900 mb-10">Our Users</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        
-        {users.map((user) => (
-          <UserCard key={user.id} {...user} />
-        ))}
-      </div>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
+        <div className="grid grid-cols-3 gap-2">
+          {posts.map((user: UserProps, index: number) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
+
+      {isModalOpen && (
+        <UserModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
     </div>
   );
 };
 
-
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
-
 
 export default Users;
